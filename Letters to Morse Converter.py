989a85c3f7b67ad2ab610dc89b8e8c45122caf2e
Jovan <abc.j.jovan@gmail.com>
2019-06-06 22:16:53 +0200

    Improve code

--- Letters to Morse Converter.py
+++ Letters to Morse Converter.py
@@ -1,78 +1,10 @@
 x = input()
+
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+morseCode = [".- ","-... ","-.-. ","-.. ",". ","..-. ","--. ",".... ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ","--.- ",".-. ","... ","- ","..- ","...- ",".-- ","-..- ","-.-- ","--.. "]
+
 for i in range(0, len(x), 1):
-	if(x[i] == "a"):
-		print(".- ", sep=' ', end='', flush=True)
-	if(x[i] == "b"):
-		print("-... ", sep=' ', end='', flush=True)
-	if(x[i] == "c"):
-		print("-.-. ", sep=' ', end='', flush=True)
-	if(x[i] == "d"):
-		print("-.. ", sep=' ', end='', flush=True)
-	if(x[i] == "e"):
-		print(". ", sep=' ', end='', flush=True)
-	if(x[i] == "f"):
-		print("..-. ", sep=' ', end='', flush=True)
-	if(x[i] == "g"):
-		print("--. ", sep=' ', end='', flush=True)
-	if(x[i] == "h"):
-		print(".... ", sep=' ', end='', flush=True)
-	if(x[i] == "i"):
-		print(".. ", sep=' ', end='', flush=True)
-	if(x[i] == "j"):
-		print(".--- ", sep=' ', end='', flush=True)
-	if(x[i] == "k"):
-		print("-.- ", sep=' ', end='', flush=True)
-	if(x[i] == "l"):
-		print(".-.. ", sep=' ', end='', flush=True)
-	if(x[i] == "m"):
-		print("-- ", sep=' ', end='', flush=True)
-	if(x[i] == "n"):
-		print("-. ", sep=' ', end='', flush=True)
-	if(x[i] == "o"):
-		print("--- ", sep=' ', end='', flush=True)
-	if(x[i] == "p"):
-		print(".--. ", sep=' ', end='', flush=True)
-	if(x[i] == "q"):
-		print("--.- ", sep=' ', end='', flush=True)
-	if(x[i] == "r"):
-		print(".-. ", sep=' ', end='', flush=True)
-	if(x[i] == "s"):
-		print("... ", sep=' ', end='', flush=True)
-	if(x[i] == "t"):
-		print("- ", sep=' ', end='', flush=True)
-	if(x[i] == "u"):
-		print("..- ", sep=' ', end='', flush=True)
-	if(x[i] == "v"):
-		print("...- ", sep=' ', end='', flush=True)
-	if(x[i] == "w"):
-		print(".-- ", sep=' ', end='', flush=True)
-	if(x[i] == "x"):
-		print("-..- ", sep=' ', end='', flush=True)
-	if(x[i] == "y"):
-		print("-.-- ", sep=' ', end='', flush=True)
-	if(x[i] == "z"):
-		print("--.. ", sep=' ', end='', flush=True)
-	if(x[i] == "1"):
-		print(".---- ", sep=' ', end='', flush=True)
-	if(x[i] == "2"):
-		print("..--- ", sep=' ', end='', flush=True)
-	if(x[i] == "3"):
-		print("...-- ", sep=' ', end='', flush=True)
-	if(x[i] == "4"):
-		print("....- ", sep=' ', end='', flush=True)
-	if(x[i] == "5"):
-		print("..... ", sep=' ', end='', flush=True)
-	if(x[i] == "6"):
-		print("-.... ", sep=' ', end='', flush=True)
-	if(x[i] == "7"):
-		print("--... ", sep=' ', end='', flush=True)
-	if(x[i] == "8"):
-		print("---.. ", sep=' ', end='', flush=True)
-	if(x[i] == "9"):
-		print("----. ", sep=' ', end='', flush=True)
-	if(x[i] == "0"):
-		print("----- ", sep=' ', end='', flush=True)
-
-
-
-
+	for e in range(0, len(letters), 1):
+		if(x[i] == letters[e]):
+			print(morseCode[e], sep=' ', end='', flush=True)
+	
