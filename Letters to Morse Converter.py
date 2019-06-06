f5445f8a04e9b7b7e4ff6dfe4bc0b5b42a7d896d
Jovan <abc.j.jovan@gmail.com>
2019-06-06 21:17:57 +0200

    Add letters

---
+++ Letters to Morse Converter.py
@@ -0,0 +1,55 @@
+x = input()
+for i in range(0, len(x), 1):
+	if(x[i] == "a"):
+		print(".- ", sep=' ', end='', flush=True)
+	if(x[i] == "b"):
+		print("-... ", sep=' ', end='', flush=True)
+	if(x[i] == "c"):
+		print("-.-. ", sep=' ', end='', flush=True)
+	if(x[i] == "d"):
+		print("-.. ", sep=' ', end='', flush=True)
+	if(x[i] == "e"):
+		print(". ", sep=' ', end='', flush=True)
+	if(x[i] == "f"):
+		print("..-. ", sep=' ', end='', flush=True)
+	if(x[i] == "g"):
+		print("--. ", sep=' ', end='', flush=True)
+	if(x[i] == "h"):
+		print(".... ", sep=' ', end='', flush=True)
+	if(x[i] == "i"):
+		print(".. ", sep=' ', end='', flush=True)
+	if(x[i] == "j"):
+		print(".--- ", sep=' ', end='', flush=True)
+	if(x[i] == "k"):
+		print("-.- ", sep=' ', end='', flush=True)
+	if(x[i] == "l"):
+		print(".-.. ", sep=' ', end='', flush=True)
+	if(x[i] == "m"):
+		print("-- ", sep=' ', end='', flush=True)
+	if(x[i] == "n"):
+		print("-. ", sep=' ', end='', flush=True)
+	if(x[i] == "o"):
+		print("--- ", sep=' ', end='', flush=True)
+	if(x[i] == "p"):
+		print(".--. ", sep=' ', end='', flush=True)
+	if(x[i] == "q"):
+		print("--.- ", sep=' ', end='', flush=True)
+	if(x[i] == "r"):
+		print(".-. ", sep=' ', end='', flush=True)
+	if(x[i] == "s"):
+		print("... ", sep=' ', end='', flush=True)
+	if(x[i] == "t"):
+		print("- ", sep=' ', end='', flush=True)
+	if(x[i] == "u"):
+		print("..- ", sep=' ', end='', flush=True)
+	if(x[i] == "v"):
+		print("...- ", sep=' ', end='', flush=True)
+	if(x[i] == "w"):
+		print(".-- ", sep=' ', end='', flush=True)
+	if(x[i] == "x"):
+		print("-..- ", sep=' ', end='', flush=True)
+	if(x[i] == "y"):
+		print("-.-- ", sep=' ', end='', flush=True)
+	if(x[i] == "z"):
+		print("--.. ", sep=' ', end='', flush=True)
+
