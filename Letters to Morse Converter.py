bd0739255731a645b66c76cbfac53144c82bb06d
Jovan <abc.j.jovan@gmail.com>
2019-06-07 23:57:27 +0200

    Minor changes

--- Letters to Morse Converter.py
+++ Letters to Morse Converter.py
@@ -1,10 +1,12 @@
-x = input()
+nastavi = True;
+while(nastavi):
+	userInput = input("Unesite tekst.")
 
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-morseCode = [".- ","-... ","-.-. ","-.. ",". ","..-. ","--. ",".... ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ","--.- ",".-. ","... ","- ","..- ","...- ",".-- ","-..- ","-.-- ","--.. "]
+	letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	morseCode = [".- ","-... ","-.-. ","-.. ",". ","..-. ","--. ",".... ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ","--.- ",".-. ","... ","- ","..- ","...- ",".-- ","-..- ","-.-- ","--.. "]
 
-for i in range(0, len(x), 1):
-	for e in range(0, len(letters), 1):
-		if(x[i] == letters[e]):
-			print(morseCode[e], sep=' ', end='', flush=True)
+	for a in range(0, len(userInput), 1):
+		for b in range(0, len(letters), 1):
+			if(userInput[a] == letters[b]):
+				print(morseCode[b], sep=' ', end='', flush=True)
 	
